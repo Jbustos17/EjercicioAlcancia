ced555761058cd17e27551a30aec26f6c65e9502
Jbustos17 <juandavidbustos04@gmail.com>
2025-02-28 22:34:33 -0500

    software

--- src/Main.kt
+++ src/Main.kt
@@ -2,43 +2,109 @@ import kotlin.random.Random
 
 fun main() {
 
-    println("Ingrese la Cantidad de dinero:")
-    val input = readLine()
-    var dinero = input?.toDoubleOrNull() ?: return println("Cantidad Invalida De Juego")// toDoubleOrNull se utiliza para convertir una cadena en un numero de punto flotante
+    var CostoCandidato1 = 0.0
+    var CostoCandidato2 = 0.0
+    var CostoCandidato3 = 0.0
+    var VotosCandidato1 = 0
+    var VotosCandidato2 = 0
+    var VotosCandidato3 = 0
+
+    val CostoInternet =700000
+    val CostoTv = 600000
+    val CostoRadio = 200000
+
+    while (true) {
+    println("1. Votar por el candidato de su elección.")
+    println("2. Calcular el costo de campaña de un candidato dependiendo de la influencia publicitaria de cada voto.")
+    println("3. Vaciar todas las urnas de la votación.")
+    println("4. Conocer el número total de votos.")
+    println("5. Porcentaje de votos obtenidos por cada candidato.")
+    println("6. Costo promedio de campaña en las elecciones")
 
 
-    while (dinero > 0){
+        when (readLine()?.toIntOrNull()) {
+            1 -> {
+                print("Ingrese el número del candidato (1, 2 o 3): ")
+                val candidato = readLine()?.toIntOrNull()
+                print("Ingrese el medio de influencia (1-Internet, 2-Radio, 3-Televisión): ")
+                val medio = readLine()?.toIntOrNull()
 
-        val numero = Random.nextInt(1,4)
-        println("Salio el numero: $numero")
-
-        when (numero){
-
-            3->{
-                println("Perdiste todo tu dinero, FIN DEL JUEGO")
-                dinero = 0.0
+                if (candidato in 1..3 && medio in 1..3) {
+                    when (candidato) {
+                        1 -> VotosCandidato1++
+                        2 -> VotosCandidato2++
+                        3 ->VotosCandidato3++
+                    }
+                    when (medio) {
+                        1 -> when (candidato) {
+                            1 -> CostoCandidato1 += CostoInternet
+                            2 -> CostoCandidato2 += CostoInternet
+                            3 -> CostoCandidato3 += CostoInternet
+                        }
+                        2 -> when (candidato) {
+                            1 -> CostoCandidato1 += CostoRadio
+                            2 -> CostoCandidato2 += CostoRadio
+                            3 -> CostoCandidato3 += CostoRadio
+                        }
+                        3 -> when (candidato) {
+                            1 -> CostoCandidato1 += CostoTv
+                            2 -> CostoCandidato2 += CostoTv
+                            3 -> CostoCandidato3 += CostoTv
+                        }
+                    }
+                } else {
+                    println("Opcion no valida")
+                }
             }
-            2-> {
-                dinero /= 2
-                println("Pierdes la mitad de tu dinero: $dinero ,Pero puedes seguir jugando")
-                println("Deseas seguir jugando (si/no)")
-                if (readLine()?. lowercase() != "si") break
-
+            2 -> {
+                print("Ingrese el numero del candidato (1, 2 , 3): ")
+                when (readLine()?.toIntOrNull()) {
+                    1 -> println("Costo campaña Candidato 1: \$${CostoCandidato1}")
+                    2 -> println("Costo campaña Candidato 2: \$${CostoCandidato2}")
+                    3 -> println("Costo campaña Candidato 3: \$${CostoCandidato3}")
+                    else -> println("Candidato no valido.")
+                }
             }
-            1->{
-                dinero *= 2
-                println("Ganaste, Duplicaste tu dinero, ahora tienes: $dinero ")
-                println("Deseas seguir jugando (si/no)")
-                if (readLine()?. lowercase() != "si") break
-
+            3 -> {
+                VotosCandidato1 = 0
+                VotosCandidato2 = 0
+                VotosCandidato3 = 0
+                CostoCandidato1 = 0.0
+                CostoCandidato2 = 0.0
+                CostoCandidato3 = 0.0
+                println("Urnas vaciadas.")
             }
+            4 -> {
+                val totalVotos = VotosCandidato1 + VotosCandidato2 + VotosCandidato3
+                println("Total de votos: $totalVotos")
+            }
+            5 -> {
+                val totalVotos = VotosCandidato1 + VotosCandidato2 + VotosCandidato3
+                if (totalVotos > 0) {
+                    println("Candidato 1: ${VotosCandidato1 * 100 / totalVotos}%")
+                    println("Candidato 2: ${VotosCandidato2 * 100 / totalVotos}%")
+                    println("Candidato 3: ${VotosCandidato3 * 100 / totalVotos}%")
+                } else {
+                    println("No hay votos registrados.")
+                }
+            }
+            6 -> {
+                val totalCosto = CostoCandidato1 + CostoCandidato2 + CostoCandidato3
+                val totalVotos = VotosCandidato1 + VotosCandidato2 + VotosCandidato3
+                if (totalVotos > 0) {
+                    println("Costo promedio de campaña: \$${totalCosto / totalVotos}")
+                } else {
+                    println("No hay votos registrados.")
+                }
+            }
+            7 -> {
+                println("Saliendo del sistema.")
+                break
+            }
+            else -> println("Opcion no valida.")
         }
-
-    println("Juego Terminado, Gracias por jugar en casino JBustos17")
-
     }
 }
-
 
 
 
